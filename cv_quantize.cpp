e64dd6e766114b9102eaf0651c07e4e11c83c49b
OSAMU WATANABE <owatanab@es.takushoku-u.ac.jp>
2021-11-24 10:39:23 +0900

    quantize/dequantize

--- cv_quantize.cpp
+++ cv_quantize.cpp
@@ -1,25 +1,50 @@
+#include <cmath>
 #include <cstdio>
 #include <opencv2/opencv.hpp>
 
-int quantize(int val, double stepsize) { return val; }
+int quantize(int val, double stepsize) {
+  int sign = 1;
+  if (val < 0) {
+    sign = -1;
+    val = -val;
+  }
+  double tmp = val / stepsize;
+  int out = (int)floorl(tmp);
+  out *= sign;
+  return out;
+}
 
-int dequantize(int qidx, double stepsize) { return qidx; }
+int dequantize(int qidx, double stepsize) {
+  double r = 0.5;
+  double tmp = qidx * stepsize;
+  if (tmp < 0) {
+    r = -0.5;
+  }
+  int out = (int)floorl(tmp + r);
+  return out;
+}
 
 using namespace cv;
 
 int main(int argc, char *argv[]) {
+  double stepsize;
   if (argc < 2) {
     printf("ERROR: an input image is required.\n");
     return EXIT_FAILURE;
   }
+
   cv::Mat image0;
   image0 = cv::imread(argv[1]);
   if (image0.empty()) {
     printf("specified image %s is not found.\n", argv[1]);
     return EXIT_FAILURE;
   }
-  double stepsize = 1.0;
-  cv::Mat image1 = image0;
+  if (argc < 3) {
+    stepsize = 1.0;
+  }
+  stepsize = atof(argv[2]);
+
+  cv::Mat image1 = image0.clone();
   int width = image0.cols;   // 画像の幅
   int height = image0.rows;  // 画像の高さ
   int ncomp = image0.channels();  // 色成分の数（カラー->3, モノクロ->1）
